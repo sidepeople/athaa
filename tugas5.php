6084cc72ec6259bc710b5cfc6b15a2004b58b74f
athaa <atha68345@gmail.com>
2025-04-28 08:48:57 +0800

    Add files via upload

---
+++ tugas5.php
@@ -0,0 +1,9 @@
+<?php
+$nilai_matematika = 85;
+$nilai_bahasa = 75;
+
+if ($nilai_matematika >= 80 && $nilai_bahasa >= 70) {
+    echo "selamat! kamu boleh nonton bioskop akhir pekan ini!";
+} else {
+    echo "yah... belajar lagi yah,lain kali bisa!";
+}
