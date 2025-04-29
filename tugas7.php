1ef5daaee0424d33fa84a3113f9e5434c9cf572c
athaa <atha68345@gmail.com>
2025-04-29 19:57:23 +0800

    tugas quiz .php

---
+++ tugas7.php
@@ -0,0 +1,11 @@
+<?php
+$bukhori="sehat wal afiat" ;
+$motor="mogok";
+
+if ($bukhori=="sehat wal afiat" && $motor=="menyala") {
+    echo "berangkat ke sekolah";
+}
+else {
+    echo "tidak berangkat karna mogok";
+}
+
