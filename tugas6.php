a302fc08fecbc6d727df2ec2c33160101b67ce98
athaa <atha68345@gmail.com>
2025-04-29 19:55:48 +0800

    tugas quiz.php

---
+++ tugas6.php
@@ -0,0 +1,18 @@
+<?php
+$Nilai_kunsyafei =94 ;
+
+if ($Nilai_kunsyafei >= 90 ) {
+    echo "sangat mampu" ;
+}
+
+elseif ($Nilai_kunsyafei >= 80) {
+    echo "baik" ;
+}
+elseif  ($Nilai_kunsyafei >= 75) {
+    echo "cukup" ;
+}
+else {
+    echo "tidak mampu" ;
+}
+
+ 
