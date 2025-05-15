dc11841a22f2686e126e9f7f882d76fd92fd7e60
athaa <atha68345@gmail.com>
2025-05-15 14:03:13 +0800

    tugas 12

---
+++ tugas12.php
@@ -0,0 +1,11 @@
+<?php
+$sepatulokal =[
+    "aerostreet",
+    "compass",
+    "ortus",
+    "specs"
+];
+echo "daftar sepatu </br>";
+foreach ($sepatulokal as $daftarsepatu) {
+    echo "-" .$daftarsepatu . "</br>" ;
+}    
